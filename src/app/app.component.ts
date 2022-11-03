a2390303c706dd1573b02e1261b0c5099f828d67
micha4211 <micha4211@icloud.com>
2022-11-03 13:05:10 -0500

    Added Service to get quizes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+import { QuizService } from './quiz.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'quiz-editor';
+
+  constructor(private quizSVC: QuizService) {};
+
+  ngOnInit() {
+    const data = this.quizSVC.loadQuizzes();
+    console.log(data);
+  }
 }
